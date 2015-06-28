30c0be31ef710186aec8491ca1c214a90c4672c3
Lindsay Hertz <lindsay.hertz@gmail.com>
2015-06-28 13:33:33 -0700

    update image links, add first img to home.html

--- src/app.js
+++ src/app.js
@@ -27,18 +27,19 @@ app.config(function($routeProvider){
 
 app.controller('SiteController', function($scope){
   $scope.imageCollection = [
-    'web/001.jpg',
-    'web/002.jpg',
-    'web/003.jpg',
-    'web/004.jpg',
-    'web/005.jpg',
-    'web/006.jpg',
-    'web/007.jpg',
-    'web/008.jpg',
-    'web/009.jpg',
-    'web/010.jpg',
-    'web/011.jpg',
-    'web/012.jpg',
-    'web/013.jpg',
+    'img/web_001.jpg',
+    'img/web_002.jpg',
+    'img/web_003.jpg',
+    'img/web_004.jpg',
+    'img/web_005.jpg',
+    'img/web_006.jpg',
+    'img/web_007.jpg',
+    'img/web_008.jpg',
+    'img/web_009.jpg',
+    'img/web_010.jpg',
+    'img/web_011.jpg',
+    'img/web_012.jpg',
+    'img/web_013.jpg',
+    'img/web_014.jpg'
   ]    
 });
