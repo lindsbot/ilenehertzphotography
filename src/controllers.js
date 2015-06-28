5279c2620b6b8e07a247c6529405f84a469f5443
Lindsay Hertz <lindsay.hertz@gmail.com>
2015-06-28 12:01:33 -0700

    first pass at new site on develop

---
+++ src/controllers.js
@@ -0,0 +1,20 @@
+angular.module('ilenePhotoSite')
+  .controller('SiteController', function($scope){
+    console.log('inside SiteController')
+    $scope.imageCollection = [
+      'web/001.jpg',
+      'web/002.jpg',
+      'web/003.jpg',
+      'web/004.jpg',
+      'web/005.jpg',
+      'web/006.jpg',
+      'web/007.jpg',
+      'web/008.jpg',
+      'web/009.jpg',
+      'web/010.jpg',
+      'web/011.jpg',
+      'web/012.jpg',
+      'web/013.jpg',
+    ]
+    
+  });
