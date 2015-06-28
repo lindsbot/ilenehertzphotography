d41746ed0dcfb64ff2da9feb45db4583348e865a
Lindsay Hertz <lindsay.hertz@gmail.com>
2015-06-28 13:13:42 -0700

    simplify view code, set up ngRoute

--- src/app.js
+++ src/app.js
@@ -1,22 +1,44 @@
-angular.module('ilenePhotoSite', [])
+var app = angular.module('ilenePhotoSite', ['ngRoute']);
 
-  .controller('SiteController', function($scope){
+app.config(function($routeProvider){
+  $routeProvider
+    .when('/', {
+      templateUrl: 'src/views/home.html'
+    })
+    .when('/about', {
+      templateUrl: 'src/views/about.html'
+    })
+    .when('/gallery', {
+      templateUrl: 'src/views/gallery.html'
+    })
+    .when('/technique', {
+      templateUrl: 'src/views/technique.html'
+    })
+    .when('/blog', {
+      templateUrl: 'src/views/blog.html'
+    })
+    .when('/contact', {
+      templateUrl: 'src/views/contact.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
 
-    $scope.imageCollection = [
-      'web/001.jpg',
-      'web/002.jpg',
-      'web/003.jpg',
-      'web/004.jpg',
-      'web/005.jpg',
-      'web/006.jpg',
-      'web/007.jpg',
-      'web/008.jpg',
-      'web/009.jpg',
-      'web/010.jpg',
-      'web/011.jpg',
-      'web/012.jpg',
-      'web/013.jpg',
-    ]
-    
-  })
-  ;
+app.controller('SiteController', function($scope){
+  $scope.imageCollection = [
+    'web/001.jpg',
+    'web/002.jpg',
+    'web/003.jpg',
+    'web/004.jpg',
+    'web/005.jpg',
+    'web/006.jpg',
+    'web/007.jpg',
+    'web/008.jpg',
+    'web/009.jpg',
+    'web/010.jpg',
+    'web/011.jpg',
+    'web/012.jpg',
+    'web/013.jpg',
+  ]    
+});
--- src/directives.js
+++ src/directives.js
@@ -1,42 +1,5 @@
 var ilenePhotoSite = angular.module('ilenePhotoSite');
 
-ilenePhotoSite.directive('navigationHeader', function(){
-  return {
-    restrict: 'E',
-    template: '<div class="navigation-header-container"> \
-                <div class="header-name">Ilene Hertz Photography</div> \
-                <navigation-links></navigation-links> \
-              </div>',
-    controller: function($scope, $element, $attrs){
-      console.log('inside navigationHeader controller');
-    },
-    link: function(scope, element, attrs){
-      console.log('inside navigationHeader link function');
-    }
-  }
-});
-
-ilenePhotoSite.directive('navigationLinks', function(){
-  return {
-    restrict: 'E',
-    template: '<div> \
-                <ul class="nav-links"> \
-                  <li>Home</li> \
-                  <li>About</li> \
-                  <li>Technique</li> \
-                  <li>Blog</li> \
-                  <li>Contact</li> \
-                </ul> \
-              </div>',
-    controller: function($scope, $element, $attrs){
-      console.log('inside navigationLinks controller');
-    },
-    link: function(scope, element, attrs){
-      console.log('inside navigationLinks link function');
-    }
-  }
-});
-
 ilenePhotoSite.directive('galleryScroll', function(){
   return {
     restrict: 'E',
@@ -48,26 +11,6 @@ ilenePhotoSite.directive('galleryScroll', function(){
     },
     link: function(scope, element, attrs){
       console.log('inside galleryScroll link function');
-    }
-  }
-});
-
-ilenePhotoSite.directive('footerSocialLinks', function(){
-  return {
-    restrict: 'E',
-    template: '<div class="social-links"> \
-                <div>Connect with me</div> \
-                <ul> \
-                  <li class="entypo social-500px"></li> \
-                  <li class="entypo social-flickr"></li> \
-                  <li class="entypo social-twitter"></li> \
-                </ul> \
-              </div>',
-    controller: function($scope, $element, $attrs){
-      console.log('inside footerSocialLinks controller');
-    },
-    link: function(scope, element, attrs){
-      console.log('inside footerSocialLinks link function');
     }
   }
 });
